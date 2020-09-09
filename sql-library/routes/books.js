4290b41974f4cf7ef93f6be2cf7ab7c3d71289ee
Zach Smith <rysskismith@gmail.com>
2020-09-09 14:02:09 -0500

    trying to get delete to work

--- sql-library/routes/books.js
+++ sql-library/routes/books.js
@@ -44,6 +44,19 @@ router.post('/:id/update', asyncHandler(async (req, res) => {
   res.redirect("/books");
 }));
 
+/* Asking for delete confirmation */
+router.get("/:id/delete", asyncHandler(async (req, res) => {
+  const book = await Book.findByPk(req.params.id);
+  res.render("books/delete", { book, title: "Delete Book" });
+}));
+
+/* Delete book from the database. */
+router.post('/:id/delete', asyncHandler(async (req ,res) => {
+  const book = await Book.findByPk(req.params.id)
+  await book.destroy();
+  res.redirect("/books");
+}));
+
 
 module.exports = router;
 
